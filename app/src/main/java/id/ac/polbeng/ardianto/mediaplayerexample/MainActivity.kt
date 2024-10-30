2c8d6ce081f74cc2ef966c7029fda80b34b4fdb6
DimasRioArdianto <daffaprasetyo2017@gmail.com>
2024-10-30 15:23:57 +0700

    Update commit1

--- app/src/main/java/id/ac/polbeng/ardianto/mediaplayerexample/MainActivity.kt
+++ app/src/main/java/id/ac/polbeng/ardianto/mediaplayerexample/MainActivity.kt
@@ -1,20 +1,61 @@
 package id.ac.polbeng.ardianto.mediaplayerexample
 
+import android.media.MediaPlayer
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import id.ac.polbeng.ardianto.mediaplayerexample.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private var mediaPlayer: MediaPlayer? = null
+    private var pause = false
+    private var position = 0
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.btnStart.setOnClickListener {
+            if (pause) {
+                mediaPlayer?.seekTo(position)
+                mediaPlayer?.start()
+                pause = false
+
+            } else {
+                if (mediaPlayer == null) {
+                    mediaPlayer = MediaPlayer.create(applicationContext, R.raw.sound)
+                    mediaPlayer?.setOnCompletionListener { stopPlayer() }
+                }
+                mediaPlayer?.start()
+
+            }
         }
+        binding.btnPause.setOnClickListener {
+            if(mediaPlayer != null){
+                position = mediaPlayer!!.getCurrentPosition()
+                mediaPlayer?.pause()
+                pause = true
+            }
+        }
+        binding.btnStop.setOnClickListener {
+            stopPlayer()
+        }
+    }
+    private fun stopPlayer() {
+        if(mediaPlayer != null){
+            pause = false
+            position = 0
+            mediaPlayer?.release()
+            mediaPlayer = null;
+            Toast.makeText(this, "Media Player Released!",
+                Toast.LENGTH_SHORT).show()
+        }
+    }
+    override fun onStop() {
+        super.onStop()
+        stopPlayer()
     }
 }
